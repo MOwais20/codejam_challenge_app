aa5a34df53d234e1c2ad24f01473cbfd2cd29e44
mowais20 <m.owais1045@gmail.com>
2023-01-06 15:58:19 +0500

    ui improved

--- src/components/FoodByCategory.js
+++ src/components/FoodByCategory.js
@@ -7,8 +7,6 @@ import { ref, set, get, child, onValue } from "firebase/database";
 import axios from "axios";
 import FoodCard from "./FoodCard";
 
-import FoodCard from "./FoodCard";
-
 const FoodByCategory = () => {
   function store_in_db(userId, data) {
     // Get logged In user ID.
@@ -24,66 +22,68 @@ const FoodByCategory = () => {
 
   const [data, setData] = React.useState([
     {
-      "id": 208670,
-      "title": "Burger",
-      "image": "https://spoonacular.com/productImages/208670-312x231.jpeg",
-      "imageType": "jpeg"
+      id: 208670,
+      title: "Burger",
+      image: "https://spoonacular.com/productImages/208670-312x231.jpeg",
+      imageType: "jpeg",
     },
     {
-      "id": 19686,
-      "title": "C.F. Burger Creamery Ultra Pasteurized Half &amp; Half, 1 Pint",
-      "image": "https://spoonacular.com/productImages/19686-312x231.jpeg",
-      "imageType": "jpeg"
+      id: 19686,
+      title: "C.F. Burger Creamery Ultra Pasteurized Half &amp; Half, 1 Pint",
+      image: "https://spoonacular.com/productImages/19686-312x231.jpeg",
+      imageType: "jpeg",
     },
     {
-      "id": 19693,
-      "title": "C.F. Burger Creamery Ultra Pasteurized Half &amp; Half, 1 Quart",
-      "image": "https://spoonacular.com/productImages/19693-312x231.jpeg",
-      "imageType": "jpeg"
+      id: 19693,
+      title: "C.F. Burger Creamery Ultra Pasteurized Half &amp; Half, 1 Quart",
+      image: "https://spoonacular.com/productImages/19693-312x231.jpeg",
+      imageType: "jpeg",
     },
     {
-      "id": 156955,
-      "title": "C.F. Burger Creamery Ultra-Pasteurized Heavy Whipping Cream, 8 Fl. Oz.",
-      "image": "https://spoonacular.com/productImages/156955-312x231.jpeg",
-      "imageType": "jpeg"
+      id: 156955,
+      title:
+        "C.F. Burger Creamery Ultra-Pasteurized Heavy Whipping Cream, 8 Fl. Oz.",
+      image: "https://spoonacular.com/productImages/156955-312x231.jpeg",
+      imageType: "jpeg",
     },
     {
-      "id": 16503,
-      "title": "Hamburger Helper, Cheesy Ranch Burger, 5.9 oz box",
-      "image": "https://spoonacular.com/productImages/16503-312x231.png",
-      "imageType": "png"
+      id: 16503,
+      title: "Hamburger Helper, Cheesy Ranch Burger, 5.9 oz box",
+      image: "https://spoonacular.com/productImages/16503-312x231.png",
+      imageType: "png",
     },
     {
-      "id": 80574,
-      "title": "Bubba Burger Original,  4lb",
-      "image": "https://spoonacular.com/productImages/80574-312x231.jpeg",
-      "imageType": "jpeg"
+      id: 80574,
+      title: "Bubba Burger Original,  4lb",
+      image: "https://spoonacular.com/productImages/80574-312x231.jpeg",
+      imageType: "jpeg",
     },
     {
-      "id": 598955,
-      "title": "Qrunch Burger Green Chile, 4 Pack, Case of 6",
-      "image": "https://spoonacular.com/productImages/598955-312x231.jpeg",
-      "imageType": "jpeg"
+      id: 598955,
+      title: "Qrunch Burger Green Chile, 4 Pack, Case of 6",
+      image: "https://spoonacular.com/productImages/598955-312x231.jpeg",
+      imageType: "jpeg",
     },
     {
-      "id": 513777,
-      "title": "Sour Gummy Burger - Mini Gummi Hamburger 60 pieces, Each order has 60 individually wrapped gummi sour burgers By EFruitti",
-      "image": "https://spoonacular.com/productImages/513777-312x231.jpeg",
-      "imageType": "jpeg"
+      id: 513777,
+      title:
+        "Sour Gummy Burger - Mini Gummi Hamburger 60 pieces, Each order has 60 individually wrapped gummi sour burgers By EFruitti",
+      image: "https://spoonacular.com/productImages/513777-312x231.jpeg",
+      imageType: "jpeg",
     },
     {
-      "id": 110007,
-      "title": "Bubba Burger® Reduced Fat Burgers 8 ct Box",
-      "image": "https://spoonacular.com/productImages/110007-312x231.jpeg",
-      "imageType": "jpeg"
+      id: 110007,
+      title: "Bubba Burger® Reduced Fat Burgers 8 ct Box",
+      image: "https://spoonacular.com/productImages/110007-312x231.jpeg",
+      imageType: "jpeg",
     },
     {
-      "id": 192864,
-      "title": "Adams Burgers, Fries &amp; More Multi-Seasoning, 5.54 oz",
-      "image": "https://spoonacular.com/productImages/192864-312x231.jpeg",
-      "imageType": "jpeg"
-    }
-  ])
+      id: 192864,
+      title: "Adams Burgers, Fries &amp; More Multi-Seasoning, 5.54 oz",
+      image: "https://spoonacular.com/productImages/192864-312x231.jpeg",
+      imageType: "jpeg",
+    },
+  ]);
   React.useEffect(() => {
     store_in_db();
     // // axios.get('https://api.spoonacular.com/food/products/search?query=pizza&apiKey=f420c936bac54c61b8a9bb6d6d04e525').then((res) => {
@@ -103,18 +103,30 @@ const FoodByCategory = () => {
               Food By Category
             </h1>
 
-            <div
+            {/* <div
               className="flex flex-wrap align-center"
               style={{ justifyContent: "space-around" }}
             >
               {[1, 2, 3, 4].map((el) => (
                 <FoodCard />
               ))}
-            </div>
+            </div> */}
           </div>
-          {data.map((object) => {
-            return <FoodCard image={object?.image} id={object?.id} title={object?.title} />
-          })}
+          <div
+            className="flex flex-wrap align-center"
+            style={{ justifyContent: "space-around" }}
+          >
+            {data.map((object, obj_index) => {
+              return (
+                <FoodCard
+                  key={obj_index}
+                  image={object?.image}
+                  id={object?.id}
+                  title={object?.title}
+                />
+              );
+            })}
+          </div>
         </Box>
       </Container>
     </React.Fragment>
--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,23 +1,42 @@
-import React from "react"
-import Tilt from 'react-vanilla-tilt'
-import LikeButton from "./button"
+import React from "react";
+import Tilt from "react-vanilla-tilt";
+import LikeButton from "./button";
 const FoodCard = (props) => {
-    return (
-        <>
-            <Tilt className="w-2/12 border" id={props?.id} style={{ borderRadius: '10px', backgroundColor: "white", boxShadow: '10px  10px 10px 0px gray' }}>
-                <div className="h-max">
-                    <img height={100} width={100} className="mx-auto py-10" style={{ objectFit: "contain" }}  alt="food image" src={props?.image} />
-                    <h1 className="text-gray-500 text-center underline mb-2">{props?.title}</h1>
-                    {/* <Card> */}
-                    <div className="flex items-center justify-center py-5">
-                        <span className="text-red-400 font-normal">Add to Favorite :</span>
-                        <LikeButton />
-                    </div>
-                    {/* </Card> */}
-                </div>
-            </Tilt>
-        </>
-    )
-}
+  return (
+    <>
+      <Tilt
+        className="w-52 border m-2"
+        id={props?.id}
+        style={{
+          borderRadius: "10px",
+          backgroundColor: "white",
+          boxShadow: "10px 10px 10px 0px gray",
+        }}
+      >
+        <div className="h-max p-2">
+          <div className="w-full border-solid border-2 border-gray-200 ">
+            <img
+              height={100}
+              width={100}
+              className="mx-auto py-10"
+              style={{ objectFit: "contain" }}
+              alt="food image"
+              src={props?.image}
+            />
+          </div>
+          <h1 className="text-gray-500 text-center font-bold text-slate-700	 mb-2">
+            {props?.title?.substring(5, 45) + "..."}
+          </h1>
+          {/* <Card> */}
+          <div className="flex items-center justify-center py-5">
+            <span className="text-red-400 font-normal">Add to Favorite </span>
+            <LikeButton />
+          </div>
+          {/* </Card> */}
+        </div>
+      </Tilt>
+    </>
+  );
+};
 
-export default FoodCard
+export default FoodCard;
