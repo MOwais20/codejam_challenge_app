e44c853a222bed18232e885e81cc84cdd6aa34c6
mowais20 <m.owais1045@gmail.com>
2023-01-06 12:45:49 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import db from "./config/firebase";
 
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Home />
+
+      {/* <Login /> */}
     </div>
   );
 }
