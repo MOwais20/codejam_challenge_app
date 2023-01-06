82e724fb2b80009b7a6fbc858e93e4aa150e7310
Same3rAijaz <aijazsameer5@gmail.com>
2023-01-06 17:01:49 +0500

    food updated

--- src/components/FoodByCategory.js
+++ src/components/FoodByCategory.js
@@ -8,7 +8,7 @@ import FoodCard from "./FoodCard";
 import Heart from "react-heart"
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 const FoodByCategory = () => {
   function store_in_db(userId, data) {
     // Get logged In user ID.
@@ -22,81 +22,18 @@ const FoodByCategory = () => {
     }
   }
 
-  const [data, setData] = React.useState([
-    {
-      id: 208670,
-      title: "Burger",
-      image: "https://spoonacular.com/productImages/208670-312x231.jpeg",
-      imageType: "jpeg",
-    },
-    {
-      id: 19686,
-      title: "C.F. Burger Creamery Ultra Pasteurized Half &amp; Half, 1 Pint",
-      image: "https://spoonacular.com/productImages/19686-312x231.jpeg",
-      imageType: "jpeg",
-    },
-    {
-      id: 19693,
-      title: "C.F. Burger Creamery Ultra Pasteurized Half &amp; Half, 1 Quart",
-      image: "https://spoonacular.com/productImages/19693-312x231.jpeg",
-      imageType: "jpeg",
-    },
-    {
-      id: 156955,
-      title:
-        "C.F. Burger Creamery Ultra-Pasteurized Heavy Whipping Cream, 8 Fl. Oz.",
-      image: "https://spoonacular.com/productImages/156955-312x231.jpeg",
-      imageType: "jpeg",
-    },
-    {
-      id: 16503,
-      title: "Hamburger Helper, Cheesy Ranch Burger, 5.9 oz box",
-      image: "https://spoonacular.com/productImages/16503-312x231.png",
-      imageType: "png",
-    },
-    {
-      id: 80574,
-      title: "Bubba Burger Original,  4lb",
-      image: "https://spoonacular.com/productImages/80574-312x231.jpeg",
-      imageType: "jpeg",
-    },
-    {
-      id: 598955,
-      title: "Qrunch Burger Green Chile, 4 Pack, Case of 6",
-      image: "https://spoonacular.com/productImages/598955-312x231.jpeg",
-      imageType: "jpeg",
-    },
-    {
-      id: 513777,
-      title:
-        "Sour Gummy Burger - Mini Gummi Hamburger 60 pieces, Each order has 60 individually wrapped gummi sour burgers By EFruitti",
-      image: "https://spoonacular.com/productImages/513777-312x231.jpeg",
-      imageType: "jpeg",
-    },
-    {
-      id: 110007,
-      title: "Bubba Burger® Reduced Fat Burgers 8 ct Box",
-      image: "https://spoonacular.com/productImages/110007-312x231.jpeg",
-      imageType: "jpeg",
-    },
-    {
-      id: 192864,
-      title: "Adams Burgers, Fries &amp; More Multi-Seasoning, 5.54 oz",
-      image: "https://spoonacular.com/productImages/192864-312x231.jpeg",
-      imageType: "jpeg",
-    },
-  ]);
+  const [data, setData] = React.useState([])
 
   const Count = useSelector((state) => state.favorite.favorite)
   console.log(Count)
   React.useEffect(() => {
+    axios.get('https://api.spoonacular.com/food/products/search?query=pizza&apiKey=557fbb441a6c4d7a9a897ea87ead25ae').then((res) => {
+      setData(res.data.products)
+      console.log(data)
+    })
     store_in_db();
-    // // axios.get('https://api.spoonacular.com/food/products/search?query=pizza&apiKey=f420c936bac54c61b8a9bb6d6d04e525').then((res) => {
-    // //   setData(res.data.products)
-    // //   console.log(data)
-    // // })
     // return () => { };
-  });
+  },[]);
 
   return (
     <React.Fragment>
