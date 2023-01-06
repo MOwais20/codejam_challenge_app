e9991c0548f22b3f639aa918b1930ff432c0ec02
Hassan <hraza66789@gmail.com>
2023-01-06 12:33:38 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route, NavLink } from 'react-router-dom'
 import db from "./config/firebase";
-
-import Home from "./components/Home";
+const Home = React.lazy(() => import('./components/Home'))
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Routes>
+        <Route
+          path = "/"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Home />
+            </React.Suspense>
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { store } from './Redux/store.js'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
