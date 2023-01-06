f3e0e60e86bfd5db71c51d2b3720ae667bf918b8
mowais20 <m.owais1045@gmail.com>
2023-01-06 17:10:07 +0500

    code updated

--- src/Redux/Reducers/favorite.reducer.js
+++ src/Redux/Reducers/favorite.reducer.js
@@ -1,57 +1,50 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const getFavoriteByID = createAsyncThunk(
-    '/userfavorite',
-    async (UserId, thunkAPI) => {
-        const response = await axios.post('/')
-        return response.data
-    }
-)
+  "/userfavorite",
+  async (UserId, thunkAPI) => {
+    const response = await axios.post("/");
+    return response.data;
+  }
+);
 const removeFavoriteByID = createAsyncThunk(
-    '/removefavorite',
-    async (UserId, ItemId, thunkAPI) => {
-        const response = await axios.post('/')
-        return response.data
-    }
-)
+  "/removefavorite",
+  async (UserId, ItemId, thunkAPI) => {
+    const response = await axios.post("/");
+    return response.data;
+  }
+);
 const AddFavoriteByID = createAsyncThunk(
-    '/addfavorite',
-    async (UserId, thunkAPI) => {
-        const response = await axios.post('/')
-        return response.data
-    }
-)
-
-
-
+  "/addfavorite",
+  async (UserId, thunkAPI) => {
+    const response = await axios.post("/");
+    return response.data;
+  }
+);
 
 const initialState = {
-    favorite: [],
-}
-
-
-
+  favorite: [],
+};
 
 export const FavoriteCounter = createSlice({
-    name: 'favorite',
-    initialState,
-    reducers: {
-        AddtoFavorite: (state, action) => {
-            state.favorite.push(action.payload)
-        },
-        RemoveFromFavorite: (state, action) => {
-            state.favorite.splice(action.payload, 1)
-        },
-    }
-    // },
-    // extraReducers: (builder) => {
-    //     builder.addCase()
-    // }
-})
+  name: "favorite",
+  initialState,
+  reducers: {
+    AddtoFavorite: (state, action) => {
+      state.favorite.push(action.payload);
+    },
+    RemoveFromFavorite: (state, action) => {
+      state.favorite.splice(action.payload, 1);
+    },
+  },
+  // },
+  // extraReducers: (builder) => {
+  //     builder.addCase()
+  // }
+});
 
 // Action creators are generated for each case reducer function
-export const { AddtoFavorite, RemoveFromFavorite } = FavoriteCounter.actions
+export const { AddtoFavorite, RemoveFromFavorite } = FavoriteCounter.actions;
 
-export default FavoriteCounter.reducer
+export default FavoriteCounter.reducer;
--- src/components/FoodByCategory.js
+++ src/components/FoodByCategory.js
@@ -5,7 +5,7 @@ import Container from "@mui/material/Container";
 import { db, auth } from "../config/firebase";
 import { ref, set, get, child, onValue } from "firebase/database";
 import FoodCard from "./FoodCard";
-import Heart from "react-heart"
+import Heart from "react-heart";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -22,32 +22,42 @@ const FoodByCategory = () => {
     }
   }
 
-  const [data, setData] = React.useState([])
+  const [data, setData] = React.useState([]);
 
-  const Count = useSelector((state) => state.favorite.favorite)
-  console.log(Count)
+  const Count = useSelector((state) => state.favorite.favorite);
+  console.log(Count);
   React.useEffect(() => {
-    axios.get('https://api.spoonacular.com/food/products/search?query=pizza&apiKey=557fbb441a6c4d7a9a897ea87ead25ae').then((res) => {
-      setData(res.data.products)
-      console.log(data)
-    })
+    axios
+      .get(
+        "https://api.spoonacular.com/food/products/search?query=pizza&apiKey=557fbb441a6c4d7a9a897ea87ead25ae"
+      )
+      .then((res) => {
+        setData(res.data.products);
+        console.log(data);
+      });
     store_in_db();
     // return () => { };
-  },[]);
+  }, []);
 
   return (
     <React.Fragment>
       <CssBaseline />
       <Container fluid="true">
         <Box sx={{ height: "100vh" }}>
-          <div style={{ width: "75vw" }} className=" text-2xl font-bold p-5 my-2 drop-shadow-sm bg-slate-200 rounded-2xl">
+          <div
+            style={{ width: "100%" }}
+            className=" text-2xl font-bold p-5 my-2 drop-shadow-sm bg-slate-200 rounded-2xl"
+          >
             <h1 className="text-2xl font-bold  drop-shadow-sm rounded-2xl">
               Food By Category
             </h1>
             <span className="float-right " style={{ marginTop: "-1.5rem" }}>
-              <Link to='/favorite'>
-                <span style={{ marginLeft: '-20px' }}>{Count.length}</span>
-                <Heart style={{ height: "1.5rem", marginTop: "-30px" }} isActive={true} />
+              <Link to="/favorite">
+                <span style={{ marginLeft: "-20px" }}>{Count.length}</span>
+                <Heart
+                  style={{ height: "1.5rem", marginTop: "-30px" }}
+                  isActive={true}
+                />
               </Link>
             </span>
             {/* <div
