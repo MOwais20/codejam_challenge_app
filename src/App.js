77eb8ed78290258cf6a1457fbc36b7f308acda1c
Hassan <hraza66789@gmail.com>
2023-01-06 12:57:15 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import db from "./config/firebase";
+import { Routes, Route, NavLink } from 'react-router-dom'
+const Home = React.lazy(() => import('./components/Home'))
 
-import Home from "./components/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Routes>
+    <Route
+      index
+      element={
+        <React.Suspense fallback={<>...</>}>
+          <Home />
+        </React.Suspense>
+      }
+    />
+  </Routes>
   );
 }
 
